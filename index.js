3ff13c0da1e60f1fffa909e08a34a12f9f34d446
mortalcanem <motalcanem@gmail.com>
2021-01-06 19:35:07 -0700

    maybe3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World'); 
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  const src = fs.createReadStream('./bigfile.txt');
+  src.on('data', data => res.write(data));
+  src.on('end', () => res.end());
+});
+server.listen(5000);
+const http = require('http');
+const formidable = require('formidable');
+
+
+console.log('something is working'); 
